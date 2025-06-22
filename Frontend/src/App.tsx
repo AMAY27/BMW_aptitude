d76502b33e53d890eca2ae934ddc59a8e301ec94
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2025-06-22 21:50:56 +0530

    Miscellaneous updates

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import DataGrid from './pages/DataGrid/DataGrid'
+     
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className="App" style={{ width: "100%", height: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+      <DataGrid />
+    </div>
   )
 }
 
---
+++ Frontend/src/pages/DataGrid/constants/actions.ts
@@ -0,0 +1,58 @@
+import { deleteCarById } from "../Services/DataService";
+
+export interface ActionItem {
+  label: string;
+  value: string;
+  handler: (rowData: any) => void;
+}
+
+export const ACTIONS: ActionItem[] = [
+  {
+    label: "Edit",
+    value: "edit",
+    handler: (rowData) => {
+      alert(`Edit ${rowData._id}`);
+    }
+  },
+  {
+    label: "Delete",
+    value: "delete",
+    handler: (rowData) => {
+      if (window.confirm(`Are you sure you want to delete ${rowData._id}?`)) {
+        try {
+          deleteCarById(rowData._id);
+          alert(`Deleted ${rowData._id}`);
+        } catch (error) {
+          console.error("Error deleting car:", error);
+          alert("Failed to delete the car. Please try again later.");
+          return;
+          
+        }
+      }
+    }
+  },
+  {
+    label: "View",
+    value: "view",
+    handler: (rowData) => {
+      alert(`View ${rowData._id}`);
+    }
+  },
+];
+
+export const queryTypesNumber = [
+  { label: "Equals", value: "isEqual" },
+  { label: "Empty", value: "isEmpty" },
+  { label: "Greater Than", value: "isGreaterThan" },
+  { label: "Less Than", value: "isLessThan" }, 
+  { label: "Greater Than Or Equal", value: "isGreaterThanOrEqual" },
+  { label: "Less Than Or Equal", value: "isLessThanOrEqual" }
+];
+
+export const queryTypesString = [
+  { label: "Equals", value: "isEqual" },
+  { label: "Starts With", value: "startsWith" },
+  { label: "Ends With", value: "endsWith" },
+  { label: "Contains", value: "contains" },
+  { label: "Empty", value: "isEmpty" }
+];
